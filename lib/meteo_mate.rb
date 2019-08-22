fa1f84ba5bff48fb10d7f02e3ea24bbbcec82ddb
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-08-21 18:15:21 -0700

    Implement utility functions for fetching forecast
    - MeteoMate#config
    - MeteoMate#get_closest_run_time
    - MeteoMate#get_forecast_time
    - MeteoMate#get_forecast_hour

--- lib/meteo_mate.rb
+++ lib/meteo_mate.rb
@@ -1,6 +1,42 @@
-require "meteo_mate/version"
+require 'meteo_mate/config'
+require 'meteo_mate/version'
 
 module MeteoMate
-  class Error < StandardError; end
-  # Your code goes here...
+
+  module Model
+
+    HRRR = 'hrrr'
+    GFS = 'gfs'
+
+  end
+
+  def self.config(model)
+    MeteoMate::Config.new(model)
+  end
+
+  def self.fetch_spot_forecast(model, time, lat, lon)
+  end
+
+  def self.fetch_forecast_files(model, time)
+  end
+
+  def self.get_closest_run_time(time, forecast_period, run_period)
+    forecast_time = MeteoMate::get_forecast_time(time, forecast_period)
+
+    run_time = Time.utc(time.year, time.month, time.day,
+      (time.hour / run_period) * run_period)
+    run_time -= run_period * 3600 if run_time == forecast_time
+
+    run_time
+  end
+
+  def self.get_forecast_time(time, forecast_period)
+    Time.utc(time.year, time.month, time.day,
+      (time.hour / forecast_period) * forecast_period)
+  end
+
+  def self.get_forecast_hour(forecast_time, run_time)
+    ((forecast_time - run_time)/3600).to_i
+  end
+
 end
--- spec/meteo_mate_spec.rb
+++ spec/meteo_mate_spec.rb
@@ -3,7 +3,28 @@ RSpec.describe MeteoMate do
     expect(MeteoMate::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "fetches a config" do
+    expect(MeteoMate::config(MeteoMate::Model::HRRR)).to be_a MeteoMate::Config
+  end
+
+  it "returns the closest run time to passed in date" do
+    # TODO: this test will be flaky if the run date is not the same as current
+    # date. Fix and make it more robust.
+    time = Time.now.utc
+    run_time = Time.utc(time.year, time.month, time.day, time.hour - 1)
+
+    expect(MeteoMate::get_closest_run_time(time, 1, 1)).to eql(run_time)
+  end
+
+  it "returns the correct forecast hour" do
+    add_hours = rand(5)
+    time = Time.now.utc
+
+    forecast_time = Time.utc(time.year, time.month, time.day,
+      time.hour - 1 + add_hours)
+    run_time = Time.utc(time.year, time.month, time.day, time.hour - 1)
+
+    expect(MeteoMate::get_forecast_hour(forecast_time, run_time))
+      .to eql(add_hours)
   end
 end
