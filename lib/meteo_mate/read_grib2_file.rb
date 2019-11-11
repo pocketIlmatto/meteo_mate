1c66bc8088c5947c419fb458866cc70ac8e27ca4
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-11-11 15:57:58 -0800

    Rename param in ReadGrib2File

--- lib/meteo_mate/read_grib2_file.rb
+++ lib/meteo_mate/read_grib2_file.rb
@@ -2,15 +2,14 @@ module MeteoMate
 
   class ReadGrib2File
 
-    def self.call(path, record, latitude: 0.0, longitude: 0.0)
+    def self.call(path, record_key, latitude: 0.0, longitude: 0.0)
       raise "#{path} not found" unless File.exist?(path)
 
       wgrib2 = MeteoMate.configuration.wgrib2_path
-      record = MeteoMate.configuration.records[record]
+      record = MeteoMate.configuration.records[record_key]
       coordinates = "#{longitude} #{latitude}"
 
       output = `#{wgrib2} #{path} -lon #{coordinates} -match "#{record}"`
-
       return if output.empty?
       # sample output:
       # "2:120674:lon=237.750000,lat=37.750000,val=282.829\n
